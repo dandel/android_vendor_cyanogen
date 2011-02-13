6d30bd6245adfd9e9fbdf5ec41cd810fb2b4da14
Bram Avontuur <bram@avontuur.org>
2011-02-13 21:39:44 +0100

    restructure prebuilt files; port wifi + g-sensor + touchscreen fixes
    Change-Id: I5306718c9b0de3c36c41fc8ae515a9c330b56e93

--- products/cyanogen_imap2x0.mk
+++ products/cyanogen_imap2x0.mk
@@ -3,14 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 # And all supported languages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit Infotmic Media Extensions.
-$(call inherit-product, device/infotm/Imapx2x0/Infotmic-MediaExtensions.mk)
-# Inherit Infotmic OpenGL ES. (Vivante)
-$(call inherit-product, device/infotm/Imapx2x0/Infotmic-OpenGLES.mk)
-# Inherit Wifi extensions.
-$(call inherit-product, device/infotm/Imapx2x0/Infotmic-Wifi.mk)
-# include GPS.
-$(call inherit-product, device/infotm/Imapx2x0/Infotmic-gps.mk)
+# Proprietary setup for hardware support
+$(call inherit-product, device/infotm/Imapx2x0/Infotmic-Proprietary.mk)
+# Generic board config
 $(call inherit-product, device/infotm/Imapx2x0/Infotmic-Board-generic.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common.mk)
@@ -31,16 +26,29 @@ PRODUCT_POLICY := android.policy_phone
 # Enable Windows Media
 WITH_WINDOWS_MEDIA := true
 
-# Include the Torch app
-PRODUCT_PACKAGES += Torch
+# Include some extra apps
+PRODUCT_PACKAGES += \
+	Torch \
+	TSCalibration
 
 # Extra glacier overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/glacier
+#PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/glacier
 
 
 # set Infotmic Properites.
+# debug.sf.hw: Use hardware accelerated GUI
+# opencore.asmd: Enable hardware video decoding (untested)
+# log.redirect-stdio: If true, get stdout/err in logcat, for debugging
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.product.manufacturer=infotm ro.board.platform=imap210
+	ro.product.manufacturer=infotm ro.board.platform=imap210 \
+	ro.sf.lcd_density=120 \
+	wifi.interface=ra0 \
+	wifi.supplicant_scan_interval=120 \
+	debug.sf.hw=1 \
+	opencore.asmd=1 \
+	debug.sensors.swap_accel=-y,x,z \
+	log.redirect-stdio=false
+
 #
 # Set ro.modversion
 #
@@ -53,7 +61,7 @@ else
 endif
 
 #
-# Copy passion specific prebuilt files
+# Copy specific prebuilt files
 #
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
